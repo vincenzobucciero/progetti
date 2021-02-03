19654fab84a386787f7b4640ef1dbc2499977872
Vincenzo Bucciero <74311987+vincenzobucciero@users.noreply.github.com>
2021-02-03 10:11:55 +0100

    Add files via upload

--- header.h
+++ header.h
@@ -1,11 +1,38 @@
 #ifndef HEADER_H_INCLUDED
 #define HEADER_H_INCLUDED
 
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "header.h"
 
-int stampa_matrice(char v1[40][40]); //prototipo della function che permette di stampare su schermo una matrice
+#define MAXUTENTI 30	//numero max di contatti nella rubrica
 
-int simulazione_cellule(char v1[40][40]); //protoripo della function che simula il cambiamento delle cellule all'aggiornarsi della matrice
+struct utente{
+    char nome[20];
+    char cognome[20];
+    char numero_telefonico[20];
+    int chiamate_eff;
+    int chiamate_ric;
+    int inseriti;
+} contatto[MAXUTENTI] ;
+
+
+
+
+int inserimento(struct utente contatto[]); //prototipo della function riguardante l'inserimento din un contatto nella rubrica
+
+void ordina_rubrica(struct utente contatto[]); //prototipo della procedura (void) riguardante l'ordinamento per cognomi dei contatti in rubrica
+
+int stampa_rubrica(struct utente contatto[]); //prototipo della function riguardante la stampa della rubrica contenente i contatti
+
+int cancellazione(struct utente contatto[]); //prototipo della function riguardante la cancellazione di un contatto dalla rubrica
+
+int visualizza_numero(struct utente contatto[], int n, char *chiave, char *nome); //prototipo della function riguardante la visualizzazione di un numero di un contatto
+
+int visualizza_chiamate(struct utente contatto[], char chiave[]); //prototipo della function riguardante la visualizzazione delle chiamate in entrata e in uscita da e verso un numero
+
+
+
 
 #endif // HEADER_H_INCLUDED
--- main.c
+++ main.c
@@ -1,33 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "header.h"
-
-
-//All'interno del main sono andato ad inserire la condizione iniziale della matrice per poi stamparla
 
 int main()
 {
-	int i=0, j;
-	int righe = 40, colonne = 40; //Variabili che indicano il numero di righe e colonne della matrice
-	char v1[40][40];       //Dichiarazione della matrice
+    int operazione; //Variabile che utilizzo nello switch per differenziare i vari casi
+    char nome[20], cognome[20], numero_telefonico[20];
 
 
-        for (i=0;i<righe;i++) //Ciclo for annidato che permette lo scorrimento di tutta la matrice
-			{
-				for(j=0;j<40;j++)
-				{
-					v1[i][j] = ' '; //Stampo in tutte le posizioni della matrice un ' ' che indica che la cellula in quella posizione è BIANCA
-				}
-			}
 
-			v1[0][19] = 'X'; //TRANNE IN QUESTA POSZIONE, v1[0][19], CHE RAPPRESENTA LA CONDIZIONE INIZIALE DELLA MATRICE, QUI VIENE STAMPATA UNA 'X',
-                            //CHE INDICA CHE LI LA CELLULA E' NERA
+    printf("              ---BENEVENUTO NELLA TUA RUBRICA---            \n\n");
 
-			stampa_matrice(v1); //Richiamo della function stampa matrice, che stamperà la trice iniziale
+	printf("        **RICORDA CHE LA RUBRICA ATTUALMENTE E' VUOTA**");
+            do {
+                printf("\n\n");
+                //Stampo a schermo le varie operazione che l'utente può eseguire
+                printf("Quale azioni vuoi eseguire?\n");
+                printf("Premi <1> per inserire un contatto nella rubrica\n");
+                printf("Premi <2> per cancellare un contatto dalla rubrica\n");
+                printf("Premi <3> per cercare un numero telefonico\n");
+                printf("Premi <4> per vedere le chiamate da e verso un numero in rubrica\n");
+                printf("Premi <5> per visualizzare i contatti presenti in rubrica\n");
+                printf("Premo <0> per uscire dalla rubrica!\n");
+
+                scanf(" %d", &operazione);
+
+                    switch (operazione) //Uso del costrutto switch per permettere la scelta dell'operazione che si vuole eseguire
+                    {
+                        case 1: printf("----> INSERIMENTO CONTATTO <----\n"); //Si va ad aggiungere un contatto nella rubrica
+                                if(inserimento(contatto)){ //Richiamo della function inserimento
+                                    ordina_rubrica(contatto); //Richiamo della function di ordinamento in modo tale che sia automattizato, nel momento in cui
+                                    						//viene inserito un contatto. automaticamente viene ordinato
+                                    }
+                            break; //Interrompo il caso 1 con un break
 
 
-		simulazione_cellule(v1); //Richiamo della function simulazione cellule che permetterà invece di stampare, sempre dopo comando dell'utente,
-                                //la stessa matrice ma aggiornata di una riga.
+                        case 2: cancellazione(contatto); //Si va a cancellare un contatto presente in rubrica
+                            break; //Interrompo il caso 2 con un break
+
+                        case 3: system("cls"); //Funzione che mi permette di pulire lo schermo
+                        		/*Per cercare un numero chiedo prima i dati del contatto all'utente*/
+								printf("Per cercare un numero, ho bisogno dei dati del contatto  \n");
+                                fflush(stdin); //Utilizzo della fflush
+								printf("Inserisci il nome del contatto:  ");    //Si chiede all'utente di inserire il nome del contatto
+                                scanf(" %s", &nome);
+                                printf("Inserisci il cognome del contatto:  "); //Si chiede all'utente di inserire il cognome del contatto
+                                scanf(" %s", &cognome);
+                                visualizza_numero(contatto,MAXUTENTI,cognome,nome); //Richiamo della function
+                                system("pause"); //Mette in pausa il programma fin quando l'utente non preme un pulsante
+
+                            break; //Interrompo il caso 3 con un break
+
+                        case 4: system("cls");
+								//Si visualizzano le chiamate effettuate e ricevute da un contatto presente in rubrica
+								printf("Inserisci il numero di telefono:  ");
+                                scanf(" %s", &numero_telefonico);
+                                visualizza_chiamate(contatto, numero_telefonico);
+                                system("pause");
+                            break; //Interrompo il caso 4 con un break
+
+
+                        case 5: stampa_rubrica(contatto); //Si va a stampare la rubrica contenente i contatti
+                            break;
+
+                        case 0: printf("Arrivederci"); //Ultimo caso in cui si voglia uscire dalla rubrica
+                                exit(0);
+                            break;
 
 }
-
+} while (operazione!=27); //Il ciclo deve continuare fin quando non si preme il tasto ESC che nel codice ASCII equivale a 27
+}
