d5c018d9e84a59be1c8d727f86d9752ac6472af4
Vincenzo Bucciero <74311987+vincenzobucciero@users.noreply.github.com>
2021-03-29 12:18:35 +0200

    Add files via upload

---
+++ Progetto - Simulazione Dinamica Cellule/header.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_H_INCLUDED
+#define HEADER_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int stampa_matrice(char v1[40][40]); //prototipo della function che permette di stampare su schermo una matrice
+
+int simulazione_cellule(char v1[40][40]); //protoripo della function che simula il cambiamento delle cellule all'aggiornarsi della matrice
+
+#endif // HEADER_H_INCLUDED
